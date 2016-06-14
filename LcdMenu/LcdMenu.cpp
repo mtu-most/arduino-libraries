2c967b1c2066f09f94092226db2329405c00631f
Bas Wijnen <wijnen@debian.org>
2016-06-14 15:06:14 -0400

    add serial debugging option

--- LcdMenu/LcdMenu.cpp
+++ LcdMenu/LcdMenu.cpp
@@ -3,40 +3,36 @@
 
 void message (char const *line1, char const *line2)
 {
-	lcd.clear ();
-	lcd.setCursor ((16 - strlen (line1)) / 2, 0);
+	DBG_MENU("message1 ", line1);
+	DBG_MENU("message2 ", line2);
+	lcd.setCursor (0, 0);
+	for (unsigned i = 0; i < 8 - strlen(line1) / 2; ++i)
+		lcd.print(" ");
 	lcd.print (line1);
-	lcd.setCursor ((16 - strlen (line2)) / 2, 1);
+	for (unsigned i = 0; i < 8 - strlen(line1) / 2 + strlen(line1); ++i)
+		lcd.print(" ");
+	lcd.setCursor (0, 1);
 	lcd.print (line2);
-}
-
-uint8_t checkButton(bool block) {
-	static bool wait = true;
-	if (block) 
-		while (lcd.readButtons () != 0) {}
-	else if (wait) {
-		if (lcd.readButtons () != 0) 
-			return 0;
-	}
-	while (true) {
-		uint8_t buttons = lcd.readButtons ();
-		if (buttons != 0) {
-			wait = true;
-				return buttons;                                          
-		}
-		wait = false;
-		if (!block)
-			return 0;
-	}
+	for (unsigned i = 0; i < 8 - strlen(line2) / 2 + strlen(line2); ++i)
+		lcd.print(" ");
+#ifdef SERIAL_LCD
+	Serial.print("Message: ");
+	Serial.print(line1);
+	Serial.print(" / ");
+	Serial.println(line2);
+#endif
 }
 
 static uint8_t getDigit (uint32_t num, uint32_t power)
 {
+	DBG_MENU("getDigit1 ", num);
+	DBG_MENU("getDigit2 ", power);
 	return (num / power) % 10;
 }
 
 static void shownum (uint8_t x, uint8_t y, uint32_t num, uint8_t digits, uint8_t decimals, uint8_t pos)
 {
+	DBG_MENU("shownum ", num);
 	lcd.setCursor (x, y);
 	uint32_t power = 1;
 	for (uint8_t t = 0; t < digits + decimals; ++t)
@@ -56,10 +52,17 @@ static void shownum (uint8_t x, uint8_t y, uint32_t num, uint8_t digits, uint8_t
 		}
 	}
 	lcd.setCursor (x + pos + (pos >= digits), y);
+#ifdef SERIAL_LCD
+	Serial.print("Show number: ");
+	Serial.print(num);
+	Serial.print("e");
+	Serial.println(decimals);
+#endif
 }
 
 uint32_t readNum (uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals, uint32_t initial)
 {
+	DBG_MENU("readnum ", initial);
 	uint8_t pos;
 	uint32_t &num = initial;
 	uint32_t power = 1;
@@ -70,7 +73,7 @@ uint32_t readNum (uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals, uint32
 	while (true)
 	{
 		shownum (x, y, num, digits, decimals, pos);
-		uint8_t b = waitForButton ();
+		uint8_t b = checkButton (true);
 		if (b & BUTTON_UP)
 		{
 			if (getDigit (num, power) == 9)
--- LcdMenu/LcdMenu.h
+++ LcdMenu/LcdMenu.h
@@ -1,8 +1,11 @@
 #ifndef _LCD_MENU
 #define _LCD_MENU
 
-//#include <Wire.h>
-#include <Adafruit_RGBLCDShield.h>
+#include <Arduino.h>
+#include "LiquidCrystal_I2C.h"
+
+//#define DBG_MENU(x, m) do { Serial.print(x); Serial.println(m); } while (0)
+#define DBG_MENU(x, m) do {} while (0)
 
 // Value to return meaning the menu should go back to the previous.
 #define BACK 127
@@ -10,20 +13,23 @@
 #define WAIT 128
 
 // You have to make sure this exists in your main program.
-extern Adafruit_RGBLCDShield lcd;
+extern LiquidCrystal_I2C lcd;
+extern uint8_t checkButton(bool block);
 
 void message(char const *line1, char const *line2 = "");
-uint8_t checkButton(bool block);
 uint32_t readNum(uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals = 0, uint32_t initial = 0);
-// Define this to override the default implementation.
-#ifndef CHECKBUTTON
-#define CHECKBUTTON checkButton
-#endif
+
+#define BUTTON_NONE 0
+#define BUTTON_LEFT 1
+#define BUTTON_UP 2
+#define BUTTON_RIGHT 4
+#define BUTTON_DOWN 8
+#define BUTTON_SELECT 16
 
 class action
 {
 	public:
-	virtual int8_t run() = 0;
+	virtual int8_t run(bool block, char const *info = "") = 0;
 };
 
 class CB_t : public action
@@ -31,7 +37,7 @@ class CB_t : public action
 	int8_t (*cb)();
 	public:
 	CB_t (int8_t(*cb)()) : cb(cb) {}
-	virtual int8_t run(bool block) { (void)&block; return cb(); }
+	virtual int8_t run(bool block, char const *info = "") { (void)&block; (void)&info; return cb(); }
 };
 
 #define MenuItem(name) int8_t name ## _impl(); CB_t name(name ## _impl); int8_t name ## _impl()
@@ -40,8 +46,8 @@ template <unsigned num_choices> class Menu : public action
 {
 	public:
 	Menu(char const *title, char const *(names)[num_choices], action *(actions)[num_choices]);
-	virtual int8_t run(bool block);
-	virtual int8_t iteration(bool block);
+	virtual int8_t run(bool block, char const *info = "");
+	virtual int8_t iteration(bool block, char const *info = "");
 
 	private:
 	char const *title;
@@ -49,60 +55,91 @@ template <unsigned num_choices> class Menu : public action
 	action **actions;
 	uint8_t current;
 	uint8_t choice;
-	void show();
+	bool waiting;
+	void show(char const *info = "");
 };
 
 template <unsigned num_choices> Menu <num_choices>::Menu(char const *title, char const *names[num_choices], action *actions[num_choices])
-	: title(title), names(names), actions(actions), current(~0), choice(0)
+	: title(title), names(names), actions(actions), current(~0), choice(0), waiting(false)
 {
 }
 
-template <unsigned num_choices> void Menu <num_choices>::show()
+template <unsigned num_choices> void Menu <num_choices>::show(char const *info)
 {
-	lcd.clear();
+	DBG_MENU("show ", title);
+	lcd.setCursor(0, 0);
 	lcd.print(title);
+	int8_t len = int8_t(strlen(info));
+	for (int8_t i = 0; i < 16 - int8_t(strlen(title)) - len; ++i)
+		lcd.print(" ");
+	if (len > 0) {
+		lcd.setCursor(16 - len, 0);
+		lcd.print(info);
+	}
 	lcd.setCursor(0,1);
+	lcd.print(choice);
+	lcd.print(":");
 	lcd.print(names[choice]);
+	for (int8_t i = 0; i < 16 - (choice >= 10 ? 3 : 2) - int8_t(strlen(names[choice])); ++i)
+		lcd.print(" ");
+#ifdef SERIAL_LCD
+	Serial.print("Menu choice: ");
+	Serial.println(names[choice]);
+#endif
 }
 
-template <unsigned num_choices> int8_t Menu <num_choices>::run()
+template <unsigned num_choices> int8_t Menu <num_choices>::run(bool block, char const *info)
 {
+	DBG_MENU("run ", title);
 	while (true)
 	{
-		if (!iteration(true))
-			return 0;
+		uint8_t ret = iteration(block, info);
+		if (ret != WAIT || !block)
+			return ret;
 	}
 }
 
-template <unsigned num_choices> int8_t Menu <num_choices>::iteration(bool block)
+template <unsigned num_choices> int8_t Menu <num_choices>::iteration(bool block, char const *info)
 {
+	DBG_MENU("iteration ", title);
 	if (waiting) {
-		uint8_t c = actions[choice]->run(block);
+		uint8_t c = actions[choice]->run(block, info);
 		if (c == WAIT) {
 			return WAIT;
+		}
 		else if (c == BACK) {
+			choice = 0;
 			waiting = false;
 			return 0;
 		}
 		choice = (choice + c) % num_choices;
 	}
 	waiting = false;
-	show();
-	uint8_t buttons = CHECKBUTTON(block);
+	show(info);
+	uint8_t buttons = checkButton(block);
 	if (!buttons) {
 		return WAIT;
 	}
-	if (buttons & BUTTON_UP)
-		choice = (choice + num_choices - 1) % num_choices;
-	else if (buttons & BUTTON_DOWN)
-		choice = (choice + 1) % num_choices;
+	if (buttons & BUTTON_UP) {
+		if (choice > 0)
+			choice -= 1;
+	}
+	else if (buttons & BUTTON_DOWN) {
+		if (choice < num_choices - 1)
+			choice += 1;
+	}
 	else if (buttons & (BUTTON_SELECT | BUTTON_RIGHT)) {
 		// Use the return value of run as the menu choice adjustment.
 		lcd.clear();
-		uint8_t c = actions[choice]->run(block);
+		uint8_t c = actions[choice]->run(block, info);
 		if (c == WAIT) {
 			waiting = true;
 			return WAIT;
+		}
+		else if (c == BACK) {
+			choice = 0;
+			waiting = false;
+			return 0;
 		}
 		choice = (choice + c) % num_choices;
 	}
