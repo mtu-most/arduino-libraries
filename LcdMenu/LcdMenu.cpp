b6d0db9f6f92e8140c08d5bd499481a3af96694b
Bas Wijnen <wijnen@debian.org>
2018-09-13 20:05:08 +0200

    support custom lcd sizes

--- LcdMenu/LcdMenu.cpp
+++ LcdMenu/LcdMenu.cpp
@@ -1,20 +1,32 @@
 #include "LcdMenu.h"
 #include <string.h>
 
-void message (char const *line1, char const *line2)
+uint8_t _lcd_width = 16, _lcd_height = 2;
+
+void message(char const *line1, char const *line2)
 {
 	DBG_MENU("message1 ", line1);
 	DBG_MENU("message2 ", line2);
-	lcd.setCursor (0, 0);
-	for (unsigned i = 0; i < 8 - strlen(line1) / 2; ++i)
+	lcd.setCursor(0, 0);
+	for (unsigned i = 0; i < _lcd_width / 2 - strlen(line1) / 2; ++i)
 		lcd.print(" ");
-	lcd.print (line1);
-	for (unsigned i = 0; i < 8 - strlen(line1) / 2 + strlen(line1); ++i)
+	lcd.print(line1);
+	for (unsigned i = 0; i < _lcd_width / 2 - strlen(line1) / 2 + strlen(line1); ++i)
 		lcd.print(" ");
-	lcd.setCursor (0, 1);
-	lcd.print (line2);
-	for (unsigned i = 0; i < 8 - strlen(line2) / 2 + strlen(line2); ++i)
+	for (unsigned y = 1; y < _lcd_height / 2; ++y) {
+		lcd.setCursor(0, y);
+		for (unsigned i = 0; i < _lcd_width; ++i)
+			lcd.print(" ");
+	}
+	lcd.setCursor(0, _lcd_height / 2);
+	lcd.print(line2);
+	for (unsigned i = 0; i < _lcd_width / 2 - strlen(line2) / 2 + strlen(line2); ++i)
 		lcd.print(" ");
+	for (unsigned y = _lcd_height / 2 + 1; y < _lcd_height; ++y) {
+		lcd.setCursor(0, y);
+		for (unsigned i = 0; i < _lcd_width; ++i)
+			lcd.print(" ");
+	}
 #ifdef SERIAL_LCD
 	Serial.print("Message: ");
 	Serial.print(line1);
@@ -23,35 +35,35 @@ void message (char const *line1, char const *line2)
 #endif
 }
 
-static uint8_t getDigit (uint32_t num, uint32_t power)
+static uint8_t getDigit(uint32_t num, uint32_t power)
 {
 	DBG_MENU("getDigit1 ", num);
 	DBG_MENU("getDigit2 ", power);
 	return (num / power) % 10;
 }
 
-static void shownum (uint8_t x, uint8_t y, uint32_t num, uint8_t digits, uint8_t decimals, uint8_t pos)
+static void shownum(uint8_t x, uint8_t y, uint32_t num, uint8_t digits, uint8_t decimals, uint8_t pos)
 {
 	DBG_MENU("shownum ", num);
-	lcd.setCursor (x, y);
+	lcd.setCursor(x, y);
 	uint32_t power = 1;
 	for (uint8_t t = 0; t < digits + decimals; ++t)
 		power *= 10;
 	for (uint8_t t = 0; t < digits; ++t)
 	{
 		power /= 10;
-		lcd.print (getDigit (num, power));
+		lcd.print(getDigit(num, power));
 	}
 	if (decimals > 0)
 	{
-		lcd.print (".");
+		lcd.print(".");
 		for (uint8_t t = 0; t < decimals; ++t)
 		{
 			power /= 10;
-			lcd.print (getDigit (num, power));
+			lcd.print(getDigit(num, power));
 		}
 	}
-	lcd.setCursor (x + pos + (pos >= digits), y);
+	lcd.setCursor(x + pos + (pos >= digits), y);
 #ifdef SERIAL_LCD
 	Serial.print("Show number: ");
 	Serial.print(num);
@@ -60,7 +72,7 @@ static void shownum (uint8_t x, uint8_t y, uint32_t num, uint8_t digits, uint8_t
 #endif
 }
 
-uint32_t readNum (uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals, uint32_t initial)
+uint32_t readNum(uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals, uint32_t initial)
 {
 	DBG_MENU("readnum ", initial);
 	uint8_t pos;
@@ -68,22 +80,22 @@ uint32_t readNum (uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals, uint32
 	uint32_t power = 1;
 	for (pos = digits + decimals - 1; pos > digits - 1; --pos)
 		power *= 10;
-	lcd.cursor ();
-	lcd.blink ();
+	lcd.cursor();
+	lcd.blink();
 	while (true)
 	{
-		shownum (x, y, num, digits, decimals, pos);
-		uint8_t b = checkButton (true);
+		shownum(x, y, num, digits, decimals, pos);
+		uint8_t b = checkButton(true);
 		if (b & BUTTON_UP)
 		{
-			if (getDigit (num, power) == 9)
+			if (getDigit(num, power) == 9)
 				num -= power * 9;
 			else
 				num += power;
 		}
 		if (b & BUTTON_DOWN)
 		{
-			if (getDigit (num, power) == 0)
+			if (getDigit(num, power) == 0)
 				num += power * 9;
 			else
 				num -= power;
@@ -104,8 +116,8 @@ uint32_t readNum (uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals, uint32
 		}
 		if (b & BUTTON_SELECT)
 		{
-			lcd.noCursor ();
-			lcd.noBlink ();
+			lcd.noCursor();
+			lcd.noBlink();
 			return num;
 		}
 	}
--- LcdMenu/LcdMenu.h
+++ LcdMenu/LcdMenu.h
@@ -2,7 +2,8 @@
 #define _LCD_MENU
 
 #include <Arduino.h>
-#include "LiquidCrystal_I2C.h"
+//#include "LiquidCrystal_I2C.h"
+#include "Adafruit_RGBLCDShield.h"
 
 //#define DBG_MENU(x, m) do { Serial.print(x); Serial.println(m); } while (0)
 #define DBG_MENU(x, m) do {} while (0)
@@ -13,18 +14,25 @@
 #define WAIT 128
 
 // You have to make sure this exists in your main program.
-extern LiquidCrystal_I2C lcd;
+//extern LiquidCrystal_I2C lcd;
+extern Adafruit_RGBLCDShield lcd;
 extern uint8_t checkButton(bool block);
+
+// Internal variables so message() will work properly.
+// Those are set by the Menu() constructor; if that is not used, they should be set manually.
+extern uint8_t _lcd_width, _lcd_height;
 
 void message(char const *line1, char const *line2 = "");
 uint32_t readNum(uint8_t x, uint8_t y, uint8_t digits, uint8_t decimals = 0, uint32_t initial = 0);
 
 #define BUTTON_NONE 0
-#define BUTTON_LEFT 1
-#define BUTTON_UP 2
-#define BUTTON_RIGHT 4
-#define BUTTON_DOWN 8
-#define BUTTON_SELECT 16
+#ifndef BUTTON_UP
+#define BUTTON_UP 0x08
+#define BUTTON_DOWN 0x04
+#define BUTTON_LEFT 0x10
+#define BUTTON_RIGHT 0x02
+#define BUTTON_SELECT 0x01
+#endif
 
 class action
 {
@@ -45,7 +53,7 @@ class CB_t : public action
 template <unsigned num_choices> class Menu : public action
 {
 	public:
-	Menu(char const *title, char const *(names)[num_choices], action *(actions)[num_choices]);
+	Menu(char const *title, char const *(names)[num_choices], action *(actions)[num_choices], uint8_t width = 16, uint8_t height = 2);
 	virtual int8_t run(bool block, char const *info = "");
 	virtual int8_t iteration(bool block, char const *info = "");
 
@@ -59,9 +67,11 @@ template <unsigned num_choices> class Menu : public action
 	void show(char const *info = "");
 };
 
-template <unsigned num_choices> Menu <num_choices>::Menu(char const *title, char const *names[num_choices], action *actions[num_choices])
+template <unsigned num_choices> Menu <num_choices>::Menu(char const *title, char const *names[num_choices], action *actions[num_choices], uint8_t width, uint8_t height)
 	: title(title), names(names), actions(actions), current(~0), choice(0), waiting(false)
 {
+	_lcd_width = width;
+	_lcd_height = height;
 }
 
 template <unsigned num_choices> void Menu <num_choices>::show(char const *info)
@@ -70,18 +80,28 @@ template <unsigned num_choices> void Menu <num_choices>::show(char const *info)
 	lcd.setCursor(0, 0);
 	lcd.print(title);
 	int8_t len = int8_t(strlen(info));
-	for (int8_t i = 0; i < 16 - int8_t(strlen(title)) - len; ++i)
+	for (int8_t i = 0; i < _lcd_width - int8_t(strlen(title)) - len; ++i)
 		lcd.print(" ");
 	if (len > 0) {
-		lcd.setCursor(16 - len, 0);
+		lcd.setCursor(_lcd_width - len, 0);
 		lcd.print(info);
 	}
-	lcd.setCursor(0,1);
+	for (int y = 1; y < _lcd_height / 2; ++y) {
+		lcd.setCursor(0, y);
+		for (int x = 0; x < _lcd_width; ++x)
+			lcd.print(" ");
+	}
+	lcd.setCursor(0, _lcd_height / 2);
 	lcd.print(choice);
 	lcd.print(":");
 	lcd.print(names[choice]);
-	for (int8_t i = 0; i < 16 - (choice >= 10 ? 3 : 2) - int8_t(strlen(names[choice])); ++i)
+	for (int8_t i = 0; i < _lcd_width - (choice >= 10 ? 3 : 2) - int8_t(strlen(names[choice])); ++i)
 		lcd.print(" ");
+	for (int y = _lcd_height / 2 + 1; y < _lcd_height; ++y) {
+		lcd.setCursor(0, y);
+		for (int x = 0; x < _lcd_width; ++x)
+			lcd.print(" ");
+	}
 #ifdef SERIAL_LCD
 	Serial.print("Menu choice: ");
 	Serial.println(names[choice]);
